2500c7e9a12ba1f38a9fabdf2cba179e524f1a09
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-06 15:29:14 +0000

    Added IVirtualMachine fake implementation

---
+++ SVM Test/FakeVirtualMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+using SVM.VirtualMachine;
+
+namespace SVM_Test
+{
+	internal class FakeVirtualMachine : IVirtualMachine
+	{
+		public int ProgramCounter => 0;
+		public Stack Stack { get; }
+
+		internal FakeVirtualMachine() => this.Stack = new Stack();
+
+		public int PopInt() => (int) this.Stack.Pop();
+	}
+}

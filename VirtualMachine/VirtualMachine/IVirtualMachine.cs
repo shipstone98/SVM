b04fd9c1a360bbc94c08df0cc72fa20e77e75beb
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-06 15:01:25 +0000

    Added IVirtualMachine interface

---
+++ VirtualMachine/VirtualMachine/IVirtualMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace SVM.VirtualMachine
+{
+	/// <summary>
+	/// Represents a virtual machine for compiling and executing interpreted code. This interface can be implemented for any stack-based instruction set.
+	/// </summary>
+	public interface IVirtualMachine
+	{
+		/// <summary>
+		/// Gets the line of the execution currently being executed.
+		/// </summary>
+		/// <value>The line of the execution currently being executed.</value>
+		/// <exception cref="InvalidOperationException">The code has not been compiled.</exception>
+		int ProgramCounter { get; }
+
+		/// <summary>
+		/// Gets the non-generic stack used by the virtual machine instance.
+		/// </summary>
+		/// <value>The non-generic stack used by the virtual machine instance.</value>
+		Stack Stack { get; }
+
+		/// <summary>
+		/// Pops an integer from the top of the <see cref="IVirtualMachine.Stack"/>.
+		/// </summary>
+		/// <returns>The integer popped from the top of the <see cref="IVirtualMachine.Stack"/>.</returns>
+		/// <exception cref="InvalidCastException">The object at the top of the stack could not be represented as a 32-bit signed integer.</exception>
+		/// <exception cref="InvalidOperationException">A stack underflow has occurred.</exception>
+		int PopInt();
+	}
+}

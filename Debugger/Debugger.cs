c2e0eb67c22a8c2eeca6cf510fd99f981b014c17
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-06 21:26:48 +0000

    Added invoker and UI thread

--- Debugger/Debugger.cs
+++ Debugger/Debugger.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Windows.Forms;
 
 using SVM.VirtualMachine;
 using SVM.VirtualMachine.Debug;
@@ -9,10 +11,44 @@ namespace Debugger
     {
         #region TASK 5 - TO BE IMPLEMENTED BY THE STUDENT
         internal bool _AwaitingForm;
+        private MainForm _Form;
+        private readonly Object _FormLock;
+        private Thread _FormThread;
 
         public IVirtualMachine VirtualMachine { internal get; set; }
 
-        public void Break(IDebugFrame debugFrame) => throw new NotImplementedException();
+        public Debugger() => this._FormLock = new Object();
+
+        public void Break(IDebugFrame debugFrame)
+		{
+            if (debugFrame is null)
+			{
+                return;
+			}
+
+            lock (this._FormLock)
+			{
+                if (this._FormThread is null)
+				{
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    this._Form = new MainForm(this);
+                    Application.Run(this._Form);
+				}
+
+                MethodInvoker invoker = delegate ()
+                {
+                    this._Form.Break(debugFrame);
+                };
+
+                this._Form.Invoke(invoker);
+
+                while (this._AwaitingForm)
+				{
+                    continue;
+				}
+			}
+		}
         #endregion
     }
 }

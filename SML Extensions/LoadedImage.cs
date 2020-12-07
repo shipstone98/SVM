0460181ef6cbcd2f76d6ed5812c3a50104c7b7bf
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-07 15:57:27 +0000

    Added base image structure

---
+++ SML Extensions/LoadedImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SML_Extensions
+{
+	public class LoadedImage
+	{
+		private readonly byte[] _RawData;
+
+		public String FileName { get; }
+		public IReadOnlyCollection<byte> RawData => this._RawData;
+
+		internal LoadedImage(String fileName, byte[] rawData)
+		{
+			this._RawData = rawData;
+			this.FileName = fileName;
+		}
+
+		public byte[] ToArray()
+		{
+			byte[] arr = new byte[this._RawData.Length];
+			Array.Copy(this._RawData, arr, arr.Length);
+			return arr;
+		}
+	}
+}

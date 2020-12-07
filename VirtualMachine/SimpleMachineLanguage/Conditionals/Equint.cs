eb2c58ba9bd46b2c3f009929a7adbf69d40995a9
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-07 21:01:33 +0000

    Moved common comparison code to abstract class

--- VirtualMachine/SimpleMachineLanguage/Conditionals/Equint.cs
+++ VirtualMachine/SimpleMachineLanguage/Conditionals/Equint.cs
@@ -1,48 +1,9 @@
-﻿using System;
-
-using SVM.VirtualMachine;
-
-namespace SVM.SimpleMachineLanguage.Conditionals
+﻿namespace SVM.SimpleMachineLanguage.Conditionals
 {
-	public class Equint : BaseInstructionWithOperand
+	public class Equint : CompareInstruction
 	{
-		public override void Run()
-		{
-			if (this.Operands is null)
-			{
-				throw new SvmRuntimeException();
-			}
+		private bool Compare(int a, int b) => a == b;
 
-			int value = this.PopInt();
-			this.VirtualMachine.Stack.Push(value);
-			int compar;
-
-			try
-			{
-				compar = Int32.Parse(this.Operands[0]);
-			}
-
-			catch
-			{
-				throw new SvmRuntimeException();
-			}
-
-			if (value == compar)
-			{
-				int line;
-
-				try
-				{
-					line = this.VirtualMachine.Labels[this.Operands[1]];
-				}
-
-				catch
-				{
-					throw new SvmRuntimeException();
-				}
-
-				this.VirtualMachine.ProgramCounter = line - 1;
-			}
-		}
+		public override void Run() => this.Run(this.Compare);
 	}
 }

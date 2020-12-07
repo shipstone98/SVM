4bd330f0feba8cd81fbb6fb8af40fd649058fa30
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-07 20:12:01 +0000

    Added equint instruction

---
+++ VirtualMachine/SimpleMachineLanguage/Conditionals/Equint.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using SVM.VirtualMachine;
+
+namespace SVM.SimpleMachineLanguage.Conditionals
+{
+	public class Equint : BaseInstructionWithOperand
+	{
+		public override void Run()
+		{
+			if (this.Operands is null)
+			{
+				throw new SvmRuntimeException();
+			}
+
+			int value = this.PopInt();
+			this.VirtualMachine.Stack.Push(value);
+			int compar;
+
+			try
+			{
+				compar = Int32.Parse(this.Operands[0]);
+			}
+
+			catch
+			{
+				throw new SvmRuntimeException();
+			}
+
+			if (value == compar)
+			{
+				int line;
+
+				try
+				{
+					line = this.VirtualMachine.Labels[this.Operands[1]];
+				}
+
+				catch
+				{
+					throw new SvmRuntimeException();
+				}
+
+				this.VirtualMachine.ProgramCounter = line - 1;
+			}
+		}
+	}
+}

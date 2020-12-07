c8928aa161559d52b972dc5f6d4e1bc557f6bc44
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-07 21:02:23 +0000

    Added instructions

---
+++ VirtualMachine/SimpleMachineLanguage/Conditionals/Bgrint.cs
@@ -0,0 +1,9 @@
+﻿namespace SVM.SimpleMachineLanguage.Conditionals
+{
+	public class Bgrint : CompareInstruction
+	{
+		private bool Compare(int a, int b) => a < b;
+
+		public override void Run() => this.Run(this.Compare);
+	}
+}

a0e16d31fd1e9116f6b2aed66e70423cc14d4fc3
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-07 15:57:49 +0000

    Added partial DisplayImage instruction

---
+++ SML Extensions/DisplayImage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using SVM.VirtualMachine;
+
+namespace SML_Extensions
+{
+	public class DisplayImage : BaseInstruction
+	{
+		public override void Run()
+		{
+			LoadedImage image;
+
+			try
+			{
+				image = (LoadedImage) this.VirtualMachine.Stack.Pop();
+			}
+
+			catch (InvalidCastException)
+			{
+				String err = String.Format(BaseInstruction.OperandOfWrongTypeMessage, this.ToString(), this.VirtualMachine.ProgramCounter);
+				throw new SvmRuntimeException(err);
+			}
+
+			catch (InvalidOperationException)
+			{
+				String err = String.Format(BaseInstruction.OperandOfWrongTypeMessage, this.ToString(), this.VirtualMachine.ProgramCounter);
+				throw new SvmRuntimeException(err);
+			}
+
+			throw new NotImplementedException();
+			//Console.WriteLine($"Image file {image.FileName} contains raw data of length {image.RawData.Count}: {System.Text.Encoding.Default.GetString(image.ToArray())}");
+		}
+	}
+}

ad98b15487a23288e9a5732f97553d1ef5d76550
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-09 22:57:42 +0000

    Added exception thrown when hash mismatch found

---
+++ VirtualMachine/VirtualMachine/InvalidHashException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SVM.VirtualMachine
+{
+	public class InvalidHashException : Exception
+	{
+		public String FileName { get; }
+
+		internal InvalidHashException(String fileName) => this.FileName = fileName;
+	}
+}

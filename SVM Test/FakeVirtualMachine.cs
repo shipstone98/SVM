15f080a89bc6a4b2fec112a9bc712010165a739d
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-12-09 17:55:11 +0000

    Added fake labelling

--- SVM Test/FakeVirtualMachine.cs
+++ SVM Test/FakeVirtualMachine.cs
@@ -8,11 +8,25 @@ namespace SVM_Test
 {
 	internal class FakeVirtualMachine : IVirtualMachine
 	{
-		public IReadOnlyDictionary<String, int> Labels => throw new NotImplementedException();
+		private readonly Dictionary<String, int> _Labels;
+
+		public IReadOnlyDictionary<String, int> Labels => this._Labels;
 		public int ProgramCounter { get; set; }
 		public Stack Stack { get; }
 
-		internal FakeVirtualMachine() => this.Stack = new Stack();
+		internal FakeVirtualMachine()
+		{
+			this._Labels = new Dictionary<String, int>();
+			this.Stack = new Stack();
+		}
+
+		internal FakeVirtualMachine(IDictionary<String, int> labels) : this()
+		{
+			foreach (KeyValuePair<String, int> label in labels)
+			{
+				this._Labels.Add(label.Key, label.Value);
+			}
+		}
 
 		public int PopInt() => (int) this.Stack.Pop();
 	}
